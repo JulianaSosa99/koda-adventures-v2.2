929a1dbb092b764addf1d1722b783ce953ebcff5
Juliana Sosa <juliana.sosa@udla.edu.ec>
2024-11-11 18:08:15 -0500

    Avance 1 -ka

---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+    [Header("Health")]
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
+
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;  // Removido el carácter inválido
+    [SerializeField] private int numberOfFlashes;    // Removido el carácter inválido
+    private SpriteRenderer spriteRend;
+
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;  // Initialize current health
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            anim.SetTrigger("hurt");
+            StartCoroutine(Invulnerability());
+        }
+        else
+        {
+            if (!dead) 
+            {
+                anim.SetTrigger("die");
+                GetComponent<PlayerController>().enabled = false;
+                dead = true;
+            }
+         
+
+        }
+    }
+    public void AddHealth(float _value)
+    { 
+        currentHealth = Mathf.Clamp(currentHealth +_value, 0, startingHealth);
+
+
+    }
+    private IEnumerator Invulnerability() 
+    
+    {
+        Physics2D.IgnoreLayerCollision(7, 8, true);
+        for (int i =0; i < numberOfFlashes; i++) 
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration/(numberOfFlashes*2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+
+
+        }
+        Physics2D.IgnoreLayerCollision(7, 8, false);
+
+
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D body;
+    [SerializeField] private float speed;
+    [SerializeField] private LayerMask groundLayer;
+    private Animator anim;
+    private BoxCollider2D boxCollider;
+
+    private float horizontalInput;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded())
+            Jump();
+
+        anim.SetBool("Walk", horizontalInput != 0);
+        anim.SetBool("grounded", isGrounded());
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    public bool canAttack()
+    {
+
+        return horizontalInput == 0 && isGrounded();
+    }
+}
